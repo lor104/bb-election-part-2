5aa6416ae01133908e44f515b52b1bf415fcd900
lor104 <lauren.rosentzveig@gmail.com>
2017-05-08 19:13:08 -0400

    add post request to vote, works if you refresh the page

--- election.js
+++ election.js
@@ -8,13 +8,33 @@ $(document).ready(function() {
     for (var i = 0; i < data.candidates.length; i++) {
       candidate = data.candidates[i]
 
-      cName = candidate.name
-      cVotes = candidate.votes
+      var cName = candidate.name;
+      var cVotes = candidate.votes;
 
-      cInfo = cName + ' has ' + cVotes + ' votes.'
+      var cInfo = cName + ' has ' + cVotes + ' votes.';
+      var submit = $('<input type="submit" value="Vote!">');
+      var hidden = $('<input type="hidden" name="id">');
+      var form = $('<form>').attr('id', cName)
 
-    $('<li>').html(cInfo).appendTo('#candidates');
+    $('<li>').html(cInfo).append(form).appendTo('#candidates');
+    $(form).attr('method', 'POST');
+    $(form).attr('action', 'https://bb-election-api.herokuapp.com/vote');
+    $(form).append(submit).append(hidden);
   }
+  });
+
+  $('body').on('submit', 'form', function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      url: $(this).attr('action'),
+      method: $(this).attr('method'),
+      data: {"name" : $(this).attr('id') },
+      dataType: 'json'
+    }).done(function(data) {
+      console.log('done on click')
+      console.log(data)
+    });
   });
 
 });
